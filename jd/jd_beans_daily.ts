45d9fe6f15b662e184a47c74ba32d232e4c0a135
Donething <donething.net@gmail.com>
2022-04-19 22:31:05 +0800

    脚本完全以 nodejs 运行，以避免导入 es6 模块，以避免出现不能在非模块应用中导入模块的问题

--- jd/jd_beans_daily.ts
+++ jd/jd_beans_daily.ts
@@ -1,7 +1,9 @@
+const axios = require('axios')
 const {USERAGENT_IOS} = require("../utils/useragent")
-const {date, request} = require("../utils/utils")
+const {date} = require("../utils/utils")
 
 // cookie
+// const jdCookie: string = process.env.JD_COOKIE || ""
 const jdCookie: string = process.env.JD_COOKIE || ""
 // 指定获取最近几天内，每日京东的变化量，不指定时为 30 天内
 const jdBeansRecentDay: number = Number(process.env.JD_BEANS_RECENT_DAY) || 30
@@ -30,31 +32,39 @@ interface JingDetailList {
   date: string          // 发生的时间，如"2022-04-18 00:14:26"
 }
 
-// 获取最近几天内，每日京东的变化量，默认 30 天内（包含）
+// 获取过去几天内，每日京东的变化量，默认 30 天内。不包含支出的京豆
 const getBeansInDay = async (day: number): Promise<Map<string, number>> => {
+  // 计算截止日期
   let expirationDate = new Date()
-  expirationDate.setDate(expirationDate.getDate() + day)
+  expirationDate.setDate(expirationDate.getDate() - day)
   let expiration = date(expirationDate, "YYYY-mm-dd")
+  console.log(`查询截止日(包含)：${expiration}`)
 
   // 按天保存京豆的变化量，如{"2022-04-18": 130, "2022-04-19": 152}
   let beansMap = new Map<string, number>()
 
+  // 初始化 每日、对应的京豆(0)
+  for (let d = 0; d < day; d++) {
+    let c = new Date()
+    c.setDate(c.getDate() - d)
+    beansMap.set(date(c, "YYYY-mm-dd"), 0)
+  }
+
+  // 开始联网获取京豆变化
   let page = 1
   while (true) {
-    let resp = await request(
-      "https://bean.m.jd.com/beanDetail/detail.json",
-      `page=${page}`,
-      {headers: headers}
-    )
+    let resp = await axios.post("https://bean.m.jd.com/beanDetail/detail.json",
+      `page=${page}`, {headers: headers})
+    let obj: BeanDetail = resp.data
 
-    let obj: BeanDetail = await resp.json()
-    if (obj.code !== "0") {
-      console.warn("获取京豆变化的详细信息失败", obj)
+    if (obj.code && obj.code !== "0") {
+      console.warn("获取京豆变化的详细信息失败：", obj)
       return beansMap
     }
 
     // 已读取完所有页
-    if (!obj.jingDetailList || obj.jingDetailList.length === 0) {
+    if (!obj.code || !obj.jingDetailList || obj.jingDetailList.length === 0) {
+      console.log("已读取完所有页，返回")
       return beansMap
     }
 
@@ -62,33 +72,45 @@ const getBeansInDay = async (day: number): Promise<Map<string, number>> => {
     for (let item of obj.jingDetailList) {
       // 提取 日期、京豆变化量
       let mdate = item.date.substring(0, item.date.indexOf(" "))
-      let num = Number(item.amount) + (beansMap.get(mdate) || 0)
-
       // 如果该日期超出了指定的天数内，就可以停止继续获取京豆变化量了
-      if (mdate > expiration) {
+      if (mdate <= expiration) {
+        return beansMap
+      }
+      // 当前日期是否在初始化日期内（可选判断）
+      if (beansMap.get(mdate) === undefined) {
+        console.log(`当前日期"${mdate}"不在初始化日期内，退出`)
         return beansMap
       }
 
+      // 不计算支出的京豆
+      if (Number(item.amount) < 0) {
+        continue
+      }
+      let num = Number(item.amount) + (beansMap.get(mdate) || 0)
       beansMap.set(mdate, num)
     }
 
     // 继续下一页
+    console.log(`已获取第 ${page} 页的京豆变化，继续获取下一页`)
     page++
   }
 }
 
 // 展示数据
-const printBeans = async () => {
+const printBeans = async (day?: number) => {
   if (!jdCookie) {
     console.log("Cookie 为空，无法获取京豆变化量")
     return
   }
 
-  let beans = await getBeansInDay(jdBeansRecentDay)
+  let beans = await getBeansInDay(day || jdBeansRecentDay)
+  let total = 0
   console.log("每日京豆变化：")
-  for (let [k, v] of Object.entries(beans)) {
-    console.log(k, v)
-  }
+  beans.forEach((v, k) => {
+    total += v
+    console.log(`${k}: ${v}`)
+  })
+  console.log(`共 ${beans.size} 天，平均每天增加 ${Math.round(total / beans.size)} 个京豆`)
 }
 
 printBeans()
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,44 +1,4 @@
 /**
- * 执行网络请求
- * 因为使用了 fetch，仅支持在类 chromium 中调用
- * @param url 目标链接
- * @param data 需要 POST 的数据
- * @param init 其它选项
- */
-exports.request = async function (url: string, data?: FormData | object | string, init?: RequestInit) {
-  let ops: RequestInit = init || {}
-
-  // 是否为POST请求
-  if (data) {
-    ops.method = "POST"
-    ops.headers = new Headers(init?.headers)
-
-    // 判断POST的数据类型
-    if (data instanceof FormData) {
-      // POST 二进制数据，不需要指定类型
-      ops.body = data
-    } else if (typeof data === "object") {
-      // POST json 数据
-      ops.body = JSON.stringify(data)
-      // 如果参数 headers 中有指定 Content-Type，则使用已指定的值，否则设为 json
-      if (!ops.headers.get("Content-Type")) {
-        ops.headers.append("Content-Type", "application/json")
-      }
-    } else {
-      // typeof data === "string"
-      // POST 表单
-      ops.body = data
-      if (!ops.headers.get("Content-Type")) {
-        // 如果参数 headers 中有指定 Content-Type，则使用已指定的值，否则设为 form
-        ops.headers.append("Content-Type", "application/x-www-form-urlencoded")
-      }
-    }
-  }
-
-  return await fetch(url, ops)
-}
-
-/**
  * 格式化日期
  * @param date 日期
  * @param fmt 返回的可读日期。默认"YYYY-mm-dd HH:MM:SS"，对应日期 "2022-03-30 22:50:39"
