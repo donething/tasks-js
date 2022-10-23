66be2a5c93587ba78085d105b9f2045e22a07dee
Donething <donething.net@gmail.com>
2022-10-23 23:40:19 +0800

    测试推送

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,6 +1,6 @@
-// 用户代理 iOS
-import {WXQiYe} from "do-utils/dist/wxpush/qiye"
+const WXQiYe = require("do-utils/dist/wxpush/qiye")
 
+// 用户代理 iOS
 exports.USERAGENT_IOS = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/99.0.4844.59 Mobile/15E148 Safari/604.1"
 
 /**
