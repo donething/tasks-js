1ba599b18e2b45b478c88e00cddf170c41b7a615
Donething <donething.net@gmail.com>
2022-11-08 17:14:19 +0800

    检测PT站港知堂是否开放注册

---
+++ tasks/discfan.ts
@@ -0,0 +1,29 @@
+/**
+ * 港知堂 开放注册
+ * 检测PT分享站“港知堂”是否开放开放注册
+ * cron 0 * * * *
+ */
+
+// 是否为青龙环境
+const isQL = !!process.env.cmd_ql
+
+const axios = require('axios')
+const {sendNotify} = require(isQL ? "./utils/sendNotify" : "../utils/sendNotify")
+
+/**
+ * 检测是否开放注册
+ */
+const check = async () => {
+  let resp = await axios.get("https://discfan.net/signup.php")
+  let text = await resp.data
+
+  if (text.indexOf("自由註冊當前關閉，目前您有三種加入社區的方式") >= 0) {
+    console.log("PT站H知堂 还未开放注册")
+    return
+  }
+
+  console.log("PT站H知堂 正在开放注册。将发送通知提醒")
+  await sendNotify("[青龙] 开放注册", "PT站H知堂 已开放注册，可以去注册了：https://discfan.net/signup.php")
+}
+
+check()
--- tasks/jd/jd_beans_daily.ts
+++ tasks/jd/jd_beans_daily.ts
@@ -1,14 +1,16 @@
 /**
+ * 京豆变化
  * 展示最近几天内京豆的变化
  * 可以指定环境变量"JD_BEANS_RECENT_DAY"，设置获取的最近天数。不指定时为 7
- * cron 5 13,22 * * * 京豆变化
+ * cron 5 13,22 * * *
  */
+
 // 是否为青龙环境
 const isQL = !!process.env.cmd_ql
 
 const axios = require('axios')
 // 根据是否是青龙环境，选择 require 路径
-const {date, notify, USERAGENT_IOS} = require(isQL ? "./utils/utils" : "../utils/utils")
+const {date, USERAGENT_IOS} = require(isQL ? "./utils/utils" : "../utils/utils")
 const {sendNotify} = require(isQL ? "./utils/sendNotify" : "../utils/sendNotify")
 
 // cookie
