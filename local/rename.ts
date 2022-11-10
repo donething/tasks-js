841b6c789eefd27c88423adec996b0083cbdab1a
Donething <donething.net@gmail.com>
2022-11-10 18:51:13 +0800

    本地功能：重命名文件

--- local/rename.ts
+++ local/rename.ts
@@ -5,20 +5,21 @@
 let path = require("path")
 let fs = require("fs")
 
-const DST = "D:/Downloads/BaiduDisk/李翰祥风月片34部"
+const DST = "D:/Test"
 const MOVIE = ".avi.mkv.mp4"
 
 let files = fs.readdirSync(DST)
-for (let p of files) {
-  let f = path.join(DST, p)
-  let subfiles = fs.readdirSync(f)
-  for (let subfile of subfiles) {
-    if (MOVIE.indexOf(path.extname(subfile)) === -1) {
-      continue
-    }
 
-    let newName = p.replace("[", "").replace("]", "")
-    console.log("重命名：", path.join(DST, p, subfile))
-    fs.renameSync(path.join(DST, p, subfile), path.join(DST, "New", newName))
+for (let f of files) {
+  let p = path.join(DST, f)
+  let stat = fs.lstatSync(p)
+
+  if (stat.isDirectory() || MOVIE.indexOf(path.extname(f)) === -1) {
+    continue
   }
+
+  // let newName = f.replace(/\(.*?\)/g, "").replace(/[\-_\s]/g, "")
+  let newName = f.replace("gg", "")
+  console.log("重命名：", p)
+  fs.renameSync(p, path.join(DST, newName))
 }
