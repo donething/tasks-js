2227c26f3d523784715134a40b098ec8a724218d
Donething <donething.net@gmail.com>
2022-11-23 17:36:28 +0800

    改变判断的文本

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 定时任务脚本，适用于 nodejs +青龙
 
-### 微信推送消息
+# 微信推送消息
 
 需要在青龙`环境变量`中添加键名`QYWX_KEY`，值为"id,secret,touser,agentid"，以英文逗号","分隔
+
+# 测试
+
+使用`TypeScript`编写的脚本，可以通过`ts-node`直接执行
+
+```shell
+ts-node .\tasks\discfan.ts
+```
--- tasks/discfan.ts
+++ tasks/discfan.ts
@@ -4,6 +4,8 @@
  * cron 0 * * * *
  */
 
+const TAG = "[PT站H知堂]"
+
 // 是否为青龙环境
 const isQL = !!process.env.cmd_ql
 
@@ -15,15 +17,19 @@ const {sendNotify} = require(isQL ? "./utils/sendNotify" : "../utils/sendNotify"
  */
 const check = async () => {
   let resp = await axios.get("https://discfan.net/signup.php")
-  let text = await resp.data
+  let text: string = await resp.data
 
   if (text.indexOf("自由註冊當前關閉") >= 0) {
-    console.log("PT站H知堂 还未开放注册")
+    console.log(TAG, "还未开放注册")
+    return
+  } else if (text.indexOf("password") >= 0) {
+    console.log(TAG, "已开放注册，将发送通知提醒")
+    await sendNotify(`[青龙] ${TAG}`, "已开放注册，可以去注册了：https://discfan.net/signup.php")
     return
   }
 
-  console.log("PT站H知堂 正在开放注册，将发送通知提醒：", text)
-  await sendNotify("[青龙] 开放注册", "PT站H知堂 已开放注册，可以去注册了：https://discfan.net/signup.php")
+  console.log(TAG, "解析网页内容出错：", text)
+  await sendNotify(`[青龙] ${TAG}`, "解析网页内容出错", text)
 }
 
 check()
