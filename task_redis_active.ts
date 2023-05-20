7ca81380a1dee2cdfb50fdfb3eaad38b5f62ca79
Donething <donething.net@gmail.com>
2023-05-20 15:45:02 +0800

    定时激活 Redis

---
+++ task_redis_active.ts
@@ -0,0 +1,21 @@
+/**
+ * 由于存储路由器的 IP 地址到 Redis 中，而过一段时间没访问时，Redis 会被删除
+ * 定时访问
+ */
+
+// new Env('激活 Redis')
+// cron: * 2 * * *
+
+const active = async (url: string) => {
+  if (!url) {
+    console.log("'ROUTER_URL'环境变量为空")
+    return
+  }
+
+  const resp = await fetch(url)
+  const text = await resp.text()
+
+  console.log("输出：", text)
+}
+
+active(process.env.ROUTER_URL || "")
