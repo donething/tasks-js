6fe4f18a3fe9cfcc8e1fa8525a3fc3749d37c7b6
Donething <donething.net@gmail.com>
2023-11-21 19:00:16 +0800

    增加馒头签到

--- task_cc_blackfriday_start.ts
+++ task_cc_blackfriday_start.ts
@@ -1,10 +1,11 @@
 /**
  * cloudcone 黑色星期五活动是否已开启
  */
-import {pushCardMsg, pushTextMsg} from "./utils/push"
 
 // new Env('cloudcone黑五活动开启')
 // cron: */10 * * * * *
+
+import {pushCardMsg, pushTextMsg} from "./utils/push"
 
 // API 的响应
 type CCResp = {
@@ -18,11 +19,13 @@ type CCResp = {
   }
 }
 
+const tag = "[青龙] CC黑五"
+
 const check = async () => {
   const response = await fetch('https://app.cloudcone.com/blackfriday/offers')
   if (!response.ok) {
     console.log("获取活动状态的响应出错：", response.statusText)
-    await pushTextMsg("[青龙] CC黑五", `获取活动状态的响应出错：\n${response.statusText}`)
+    await pushTextMsg(tag, `获取活动状态的响应出错：\n${response.statusText}`)
     return
   }
 
@@ -33,8 +36,7 @@ const check = async () => {
   }
 
   console.log("活动已开启：", JSON.stringify(data))
-  await pushCardMsg("[青龙] CC黑五", "活动已开启！",
-    "https://app.cloudcone.com/blackfriday", "点击访问")
+  await pushCardMsg(tag, "活动已开启！", "https://app.cloudcone.com/blackfriday", "点击访问")
 }
 
 check()
---
+++ task_mt_signin.ts
@@ -0,0 +1,49 @@
+/**
+ * 馒头PT 签到
+ * 先增加环境变量"MT_USER_PWD"，值用"//"来分隔用户名和密码。如"username//password"
+ * @see https://kp.m-team.cc/
+ */
+
+// new Env('馒头签到')
+// cron: * 9,21 * * *
+
+import {pushTextMsg} from "./utils/push"
+import {request} from "do-utils"
+import {UserAgents} from "./utils/utils"
+
+const tag = "[青龙] 馒头签到"
+const host = "kp.m-team.cc"
+const addr = `https://${host}`
+const loginUrl = `${addr}/takelogin.php`
+
+const loginToMT = async (username: string, password: string): Promise<void> => {
+  const headers = {
+    "authority": host,
+    "origin": addr,
+    "referer": `${addr}/login.php`,
+    "user-agent": UserAgents.Win
+  }
+
+  const data = `username=${username}&password=${password}`
+  const resp = await request(loginUrl, data, {headers})
+  const text = await resp.text()
+
+  // 不包括用户名，登录失败
+  if (!text.includes(username)) {
+    console.log("登录失败：\n", text.substring(text.indexOf("<body")))
+    await pushTextMsg(tag, "登录失败：可在脚本面板查看该脚本的执行日志")
+
+    return
+  }
+
+  // 登录成功
+  console.log('签到成功！')
+  await pushTextMsg(tag, `签到成功！`)
+}
+
+
+// 签到
+if (process.env.MT_USER_PWD) {
+  const [username, password] = process.env.MT_USER_PWD.split("//")
+  loginToMT(username, password)
+}
