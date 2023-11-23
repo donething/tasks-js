8f342a4295ea8ec514d55c4ede696c0e0e070fa5
Donething <donething.net@gmail.com>
2023-11-23 18:59:43 +0800

    增加cloudcone自动下订单

--- task_cc_blackfriday_start.ts
+++ task_cc_blackfriday_start.ts
@@ -111,7 +111,8 @@ const order = async (cookie: string, token: string, vpsInfo: VPSInfo) => {
   const resp = await request(`${addr}/ajax/vps`, data, {headers})
   const text = await resp.text()
 
-  console.log(`🤨 自动下单 ${title} ${addr}${vpsInfo.order_url}：`, `响应状态 ${resp.status}：\n`, text)
+  console.log(`🤨 自动下单 ${title} ${addr}${vpsInfo.order_url}：\n`,
+    `🤨 响应状态 ${resp.status}：\n`, text)
 }
 
 // 开始
