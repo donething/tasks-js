cea61fc5a0f1b9f2a8a99c9e9701eeb59041218f
Donething <donething.net@gmail.com>
2023-12-03 05:51:55 +0800

    尝试解决 无头浏览器无法使用

--- utils/spider/nodeseek/nodeseek.ts
+++ utils/spider/nodeseek/nodeseek.ts
@@ -19,7 +19,7 @@ const ua = "Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0"
 
 // 选项
 const options: PuppeteerLaunchOptions = {
-  headless: "new",
+  headless: false,
   defaultViewport: {width: 1920, height: 1080},
   args: ["--no-sandbox", "--disabled-setupid-sandbox"]
 }
