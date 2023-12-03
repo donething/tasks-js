a4a1ee624d8f1063c59b6ccd14a43f219680a777
Donething <donething.net@gmail.com>
2023-12-03 16:24:52 +0800

    尝试解决 无头浏览器无法使用

--- task_cc_wait_sale.ts
+++ task_cc_wait_sale.ts
@@ -26,7 +26,7 @@ const taskInfo: TaskInfo = {
   ],
 
   // 只匹配 cloudcone 有关的帖子
-  reg: /(\b(cc(?!s)\w*)\b)|(\bcloudcone\w*\b)/i,
+  reg: /(\b(\w*cc(?!s)\w*)\b)|(\b\w*cloudcone\w*\b)/i,
 
   // 保存数据的文件路径
   filepath: "./db/cc_low_price.json",
