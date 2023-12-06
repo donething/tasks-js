485be729dd2e69286945af52f312301f2fdf6cb1
Donething <donething.net@gmail.com>
2023-12-07 03:24:47 +0800

    为减少总运行时间，编译为js

---
+++ dist/task_v2ex_vps_topics.js
@@ -0,0 +1,35 @@
+"use strict";
+/**
+ * 当 v2ex 有 vps 相关的主题时，发送通知
+ */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// new Env('V2exVPS新帖')
+// cron: */2 * * * *
+const topicsFile_1 = __importDefault(require("./utils/topicsFile"));
+const v2ex_1 = __importDefault(require("./utils/spider/v2ex/v2ex"));
+const tgpush_1 = require("./utils/tgpush");
+const do_utils_1 = require("do-utils");
+const TAG = "V2exVPS";
+// 任务信息
+const taskInfo = {
+    // 需要扫描帖子的网址及节点
+    topicTaskInfos: [
+        {
+            fun: v2ex_1.default,
+            node: ""
+        }
+    ],
+    // 只匹配 VPS 有关的帖子
+    reg: /\b(vps)\b/i,
+    // 保存数据的文件路径
+    filepath: "./db/v2ex_vps_topics.json",
+    // 发送通知时的提示文本
+    tag: TAG
+};
+(0, topicsFile_1.default)(taskInfo).catch(err => {
+    console.log(TAG, "通知帖子出错：", err);
+    (0, tgpush_1.pushTGMsg)(`通知帖子出错`, do_utils_1.TGSender.escapeMk(err), TAG);
+});
