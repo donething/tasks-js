63268160dfae735a3ad96da161b07076591c6191
Donething <donething.net@gmail.com>
2023-12-09 23:48:10 +0800

    axios Error 只打印其中的 message 和 URL

--- dist/utils/comm.js
+++ dist/utils/comm.js
@@ -6,7 +6,7 @@ exports.TAG = "青龙";
 // 如果是 AxiosError 类型的错误，提取 URL 到 message 中
 const parseAxiosErr = (err) => {
     if (err instanceof axios_1.AxiosError && err.config) {
-        return { ...err, message: `${err.message} ${err.config.url}` };
+        return { ...err, message: `AxiosError ${err.message} ${err.config.url}` };
     }
     return err;
 };
