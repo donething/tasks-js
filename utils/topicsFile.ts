7b3478cfa9145230b399981804307d46a4e9bac5
Donething <donething.net@gmail.com>
2023-12-10 21:19:16 +0800

    不发送帖子的内容

--- utils/topicsFile.ts
+++ utils/topicsFile.ts
@@ -58,7 +58,8 @@ const notifyTopics = async (taskInfo: TaskInfo) => {
 
       console.log(`😊 通知新帖：`, t.title, "\n  ", t.url, "\n")
       // const ok = await pushTGTopic(taskInfo.tag, t)
-      const ok = await pushWxMsg(t.content, `${taskInfo.tag} ${t.title}`, t.url)
+      const ok = await pushWxMsg(`访问：\n\n<a href="${t.url}">${t.title}</a>`,
+        `${taskInfo.tag} ${t.title}`)
       if (!ok) {
         continue
       }
